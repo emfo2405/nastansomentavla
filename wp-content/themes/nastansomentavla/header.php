17fb0c0ef8636d7faf5c321be2e027121f1b01dd
emfo2405 <emfo2405@student.miun.se>
2026-01-03 17:09:59 +0100

    Header skapad och startsida flyttad till en php-fil

---
+++ wp-content/themes/nastansomentavla/header.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="sv">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>NSET | Startsida</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon-32x32.png">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gotu&family=Londrina+Sketch&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik+Doodle+Shadow&family=Shadows+Into+Light&family=Story+Script&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
+</head>
+
+<body>
+
+
+    <div id="headerDiv">
+                <img src="/images/Fabriksgatan-logotyp2.png" alt="Målning av ett hus på fabriksgatan">
+    </div>
+        
+    <nav id="menu">
+
+<div id="smallMenu">
+
+        <div><img src="/images/logotypMedText.png" alt="Målad bild på en makrill"></div>
+
+                <div id="mobileMenu">
+            <button id="menuButton">Meny<span id="menuIcon" class="material-symbols-outlined">menu</span></button>
+        </div>
+</div>
+        <div id="menuLinks">
+        <ul>
+            <li><a href="/home.html">Startsida</a></li>
+                        <li><a href="/contact.html">Kontakta mig</a></li>
+            <li><a href="/news.html">Nyheter</a></li>
+            <li><a href="/products.html">Produkter</a></li>
+            <li><a href="/checkout.html">Kassa</a></li>
+        </ul>
+        </div>
+        
+    </nav> 
---
+++ wp-content/themes/nastansomentavla/index.php
@@ -0,0 +1,107 @@
+  <?php get_header(); ?>
+
+    <div id="pageTitleStart">
+        <h1>Nästan som en tavla <br> -Konst inspirerad av Göteborg-</h1>
+    </div>
+    </div>
+
+
+        <div id="productsDiv">
+        <h2>-Våra senaste produkter-</h2>
+
+        <div id="productList">
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
+            </div>
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
+            </div>
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
+            </div>
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
+            </div>
+            
+        </div>
+
+        <a href="/products.html">Visa alla produkter</a>
+    </div>
+
+
+    <div id="aboutDiv">
+        <div id="aboutImg">
+            <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+        </div>
+        
+        <div id="aboutText">
+            <h2>-Om konstnären bakom motiven-</h2>
+            <p>text text text text text text </p>
+        </div>
+    </div>
+
+    
+
+
+    <div id="faq">
+        <h2>-Vanliga frågor och svar-</h2>
+        <div id="questions">
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            
+        </div>
+    </div>
+
+    <div id="news">
+        <h2>-Senaste nyheter och uppdateringar-</h2>
+        <div class="newsArticle">
+            
+            <img src="images/profilbild.jpg">
+            <div class="newsText">
+            <h3>Nyhet</h3>
+            <p>text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
+            <p>Datum</p>
+        </div>
+        </div>
+
+                <div class="newsArticle">
+            
+            <img src="images/profilbild.jpg">
+            <div class="newsText">
+            <h3>Nyhet</h3>
+            <p>text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
+            <p>Datum</p>
+        </div>
+        </div>
+
+ <a href="/products.html">Läs alla nyheter</a>
+
+    </div>
+
+
+
+    <footer>
+        <p>NSET <br> est 2022</p>
+    </footer>
+
+    <script src="/js/main.js"></script>
+</body>
+</html>
