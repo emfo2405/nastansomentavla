723d09023ac098dfc19505beabbf7ed5c3bb2101
emfo2405 <emfo2405@student.miun.se>
2026-01-04 15:49:54 +0100

    Bilder fixade och meny ändrad till en wp-länk

--- wp-content/themes/nastansomentavla/header.php
+++ wp-content/themes/nastansomentavla/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/styles.css">
     <title><?php bloginfo('name')?></title>
-    <link rel="icon" type="image/x-icon" href="/images/favicon-32x32.png">
+    <link rel="icon" type="image/x-icon" href="<?= get_template_directory_uri(); ?>/images/favicon-32x32.png">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Gotu&family=Londrina+Sketch&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Rubik+Doodle+Shadow&family=Shadows+Into+Light&family=Story+Script&display=swap" rel="stylesheet">
@@ -18,27 +18,23 @@
 
 
     <div id="headerDiv">
-                <img src="/images/Fabriksgatan-logotyp2.png" alt="Målning av ett hus på fabriksgatan">
-    </div>
+                <img src="<?= get_template_directory_uri(); ?>/images/Fabriksgatan-logotyp2.png" alt="Målning av ett hus på fabriksgatan">
+
         
     <nav id="menu">
+        
 
 <div id="smallMenu">
 
-        <div><img src="/images/logotypMedText.png" alt="Målad bild på en makrill"></div>
+        <div><a href="<?= get_home_url(); ?>"><img src="<?= get_template_directory_uri(); ?>/images/logotypMedText.png" alt="Målad bild på en makrill"></a></div>
 
                 <div id="mobileMenu">
             <button id="menuButton">Meny<span id="menuIcon" class="material-symbols-outlined">menu</span></button>
         </div>
 </div>
         <div id="menuLinks">
-        <ul>
-            <li><a href="/home.html">Startsida</a></li>
-                        <li><a href="/contact.html">Kontakta mig</a></li>
-            <li><a href="/news.html">Nyheter</a></li>
-            <li><a href="/products.html">Produkter</a></li>
-            <li><a href="/checkout.html">Kassa</a></li>
-        </ul>
+<?php wp_nav_menu(array('theme_location' => 'main-nav')); ?>
         </div>
         
     </nav> 
+    </div>
