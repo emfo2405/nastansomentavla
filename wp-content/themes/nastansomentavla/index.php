6fbd83a7f6fb8736f921a2b2006aacd234b31296
emfo2405 <emfo2405@student.miun.se>
2026-01-05 09:38:31 +0100

    underrubriker tillagda med custom fields

--- wp-content/themes/nastansomentavla/index.php
+++ wp-content/themes/nastansomentavla/index.php
@@ -1,42 +1,100 @@
-<?php get_header(); ?>
+  <?php get_header(); ?>
 
-
-    <div id="pageTitle">
-        <h1><?php the_title(); ?></h1>
+    <div id="pageTitleStart">
+        <h1><?php bloginfo('name'); ?><br><?php echo esc_html(get_post_meta(get_the_ID(), 'subtitle', true)); ?></h1>
+    </div>
     </div>
 
-        <div id="newsPage">
-        <h2><?php echo esc_html(get_post_meta(get_the_ID(), 'subtitle', true) ); ?></h2>
-       
 
-        <?php query_posts('category_name=nyheter');
-            if(have_posts()) {
-                while(have_posts()) {
-                    the_post();
-                ?>
- <div class="newsPageArticle">
-            <?php the_post_thumbnail('news-image'); ?>
-            <div class="newsPageText">
-            <h3><?php the_title(); ?></h3>
-            <p><?php the_excerpt(); ?></p>
-            <div class="lowerNews">
-            <a href='<?php the_permalink(); ?>'>Läs nyheten</a>    
-            <p><?= get_the_date(); ?></p>
-            
+        <div id="productsDiv">
+        <h2><?php echo esc_html(get_post_meta(get_the_ID(), 'productsHeading', true)); ?></h2>
+
+        <div id="productList">
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
             </div>
- </div>
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
+            </div>
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
+            </div>
+            <div class="oneProduct">
+                <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+                <p>Namn på gata</p>
+            </div>
+            
         </div>
-<?php
 
-            }
-        } 
-
-        wp_reset_query();
-        ?>
-
-                
+        <a href="/products.html">Visa alla produkter</a>
+    </div>
 
 
-   
+    <div id="aboutDiv">
+        <div id="aboutImg">
+            <img src="/images/profilbild.jpg" alt="Foto av Emma Forsmalm sittades med en kopp i händerna">
+        </div>
+        
+        <div id="aboutText">
+            <h2>-Om konstnären bakom motiven-</h2>
+            <p>text text text text text text </p>
+        </div>
+    </div>
+
+    
+
+
+    <div id="faq">
+        <h2><?php echo esc_html(get_post_meta(get_the_ID(), 'faqHeading', true)); ?></h2>
+        <div id="questions">
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            <div class="oneQuestion">
+                <h3>Fråga</h3>
+                <p>Svar svar svar svar svar svar svar svar svar svar svar svar svar</p>
+            </div>
+            
+        </div>
+    </div>
+
+    <div id="news">
+        <h2><?php echo esc_html(get_post_meta(get_the_ID(), 'newsHeading', true)); ?></h2>
+        <div class="newsArticle">
+            
+            <img src="images/profilbild.jpg">
+            <div class="newsText">
+            <h3>Nyhet</h3>
+            <p>text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
+            <p>Datum</p>
+        </div>
+        </div>
+
+                <div class="newsArticle">
+            
+            <img src="images/profilbild.jpg">
+            <div class="newsText">
+            <h3>Nyhet</h3>
+            <p>text text text text text text text text text text text text text text text text text text text text text text text text text text text text </p>
+            <p>Datum</p>
+        </div>
+        </div>
+
+ <a href="/products.html">Läs alla nyheter</a>
+
+    </div>
+
 
 <?php get_footer(); ?>
---
+++ wp-content/themes/nastansomentavla/page-news.php
@@ -0,0 +1,42 @@
+
+<?php 
+/*
+Template Name: Nyhetssida
+*/
+get_header(); ?>
+
+
+<div id="pageTitle">
+    <h1><?php the_title(); ?></h1>
+</div>
+
+<div id="newsPage">
+    <h2><?php echo esc_html(get_post_meta(get_the_ID(), 'subtitle', true)); ?></h2>
+
+
+    <?php query_posts('category_name=nyheter');
+    if (have_posts()) {
+        while (have_posts()) {
+            the_post();
+            ?>
+            <div class="newsPageArticle">
+                <?php the_post_thumbnail('news-image'); ?>
+                <div class="newsPageText">
+                    <h3><?php the_title(); ?></h3>
+                    <p><?php the_excerpt(); ?></p>
+                    <div class="lowerNews">
+                        <a href='<?php the_permalink(); ?>'>Läs nyheten</a>
+                        <p><?= get_the_date(); ?></p>
+
+                    </div>
+                </div>
+            </div>
+            <?php
+
+        }
+    }
+
+    wp_reset_query();
+    ?>
+
+    <?php get_footer(); ?>
