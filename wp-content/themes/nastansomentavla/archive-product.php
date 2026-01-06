b328b6bec1a3d27d631e61047951cc053a1d6cb9
emfo2405 <emfo2405@student.miun.se>
2026-01-06 11:07:06 +0100

    Kontaktformulärsplugin installerat och tillagt på kontaktsidan

---
+++ wp-content/themes/nastansomentavla/archive-product.php
@@ -0,0 +1,50 @@
+
+<?php 
+/*
+Template Name: Nyhetssida
+*/
+get_header(); 
+
+$page_id = get_the_ID();
+?>
+
+<div id="pageTitle">
+    <h1><?php echo get_the_title($page_id); ?></h1>
+</div>
+
+<div id="newsPage">
+    <h2><?php echo esc_html(get_post_meta(get_the_ID(), 'subtitle', true)); ?></h2>
+
+
+<div id="productList">
+    <?php query_posts('post_type=product');
+        if(have_posts()) {
+        while(have_posts()) {
+            the_post();
+
+            $product = wc_get_product(get_the_ID());
+            ?>
+        <div class="onePoster">
+            <?php the_post_thumbnail(); ?>
+            <div class="posterText">
+            <p><?php the_title(); ?></p>
+            <p><?php echo $product ? $product -> get_price_html() : ''; ?></p>
+            </div>
+            <a href="<?php echo esc_url($product -> add_to_cart_url());?>" class="buyButton">Köp</a>
+        </div>
+
+        <?php
+        } 
+        } 
+
+    wp_reset_query();
+    ?>
+            
+
+</div>
+        </div>
+
+
+
+
+    <?php get_footer(); ?>
---
+++ wp-content/themes/nastansomentavla/page-contact.php
@@ -0,0 +1,38 @@
+<?php 
+/*
+Template Name: Kontaktsida
+*/
+get_header(); ?>
+
+    <div id="pageTitle">
+        <h1><?php the_title(); ?></h1>
+    </div>
+
+    <?php query_posts('category_name=kontaktinformation');
+    if(have_posts()) {
+        while(have_posts()) {
+            the_post();
+            ?>
+
+        <div id="contactInfo">
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content();?> </p>
+    </div>
+
+<?php
+        }
+    }
+
+    wp_reset_query();
+    ?>
+
+
+    <div id="contactForm">
+        <h2><?php echo esc_html(get_post_meta(get_the_ID(), 'contactForm', true)); ?></h2>
+
+            <?php echo do_shortcode('[contact-form-7 id="6248d14" title="kontaktformulär"]'); ?>
+    </div>
+
+<?php 
+get_footer();
+?>
