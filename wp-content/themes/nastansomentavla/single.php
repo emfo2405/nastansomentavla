deb107752e37336822c59053249bc46b4fe4142a
emfo2405 <emfo2405@student.miun.se>
2026-01-06 11:17:32 +0100

    Mall för single.php skapad

---
+++ wp-content/themes/nastansomentavla/single.php
@@ -0,0 +1,31 @@
+
+<?php 
+
+get_header(); ?>
+
+
+<div id="pageTitle">
+    <h1><?php the_title(); ?></h1>
+</div>
+
+<?php
+    if(have_posts()) {
+        while(have_posts()) {
+            the_post();
+            ?>
+
+        <div id="post">
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content();?> </p>
+    </div>
+
+<?php
+        }
+    }
+
+    wp_reset_query();
+    ?>
+
+
+
+    <?php get_footer(); ?>
