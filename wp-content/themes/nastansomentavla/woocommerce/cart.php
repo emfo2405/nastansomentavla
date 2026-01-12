bdf4dd2a9db8efac82cdcca5e3f806d551eb9f18
emfo2405 <emfo2405@student.miun.se>
2026-01-12 15:11:42 +0100

    Struktur för kassa och varukorg uppdaterad samt extra css tillagd till dem

--- wp-content/themes/nastansomentavla/woocommerce/cart.php
+++ wp-content/themes/nastansomentavla/woocommerce/cart.php
@@ -1,20 +1,34 @@
 
 <?php 
+/*
+Template Name: Varukorg
+*/
 
-get_header(); 
+get_header(); ?>
 
-?>
 
 <div id="pageTitle">
     <h1><?php the_title(); ?></h1>
 </div>
 
-<div class="cartFunction">
-<?php if (functions_exists('woocommerce_cart')) {
-    woocommerce_cart();
-}
-?>
-</div>
+<?php
+    if(have_posts()) {
+        while(have_posts()) {
+            the_post();
+            ?>
+
+        <div id="cartPost">
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content();?> </p>
+    </div>
+
+<?php
+        }
+    }
+
+    wp_reset_query();
+    ?>
+
 
 
     <?php get_footer(); ?>
--- wp-content/themes/nastansomentavla/woocommerce/checkout.php
+++ wp-content/themes/nastansomentavla/woocommerce/checkout.php
@@ -1,20 +1,34 @@
 
 <?php 
+/*
+Template Name: Kassa
+*/
 
-get_header(); 
+get_header(); ?>
 
-?>
 
 <div id="pageTitle">
     <h1><?php the_title(); ?></h1>
 </div>
 
-<div class="checkoutFunction">
-<?php if (functions_exists('woocommerce_checkout')) {
-    woocommerce_checkout();
-}
-?>
-</div>
+<?php
+    if(have_posts()) {
+        while(have_posts()) {
+            the_post();
+            ?>
+
+        <div id="checkoutPost">
+        <h2><?php the_title(); ?></h2>
+        <p><?php the_content();?> </p>
+    </div>
+
+<?php
+        }
+    }
+
+    wp_reset_query();
+    ?>
+
 
 
     <?php get_footer(); ?>
